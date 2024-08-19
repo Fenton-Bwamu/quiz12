0d2edaa78ffe3c5b32d7240f19b013137d9887f9
fentoncollins123456789 <fentoncollins29@gmail.com>
2024-08-19 14:34:23 +0300

    first commit

---
+++ quiz1.js
@@ -0,0 +1,10 @@
+function checkEvenOdd(number){
+    
+if (number % 2 === 0) {
+console.log("Even");
+
+ } else {
+    console.log("Odd");
+ }
+}
+checkEvenOdd(3);
---
+++ quiz2.js
@@ -0,0 +1,16 @@
+function checksign(number) {
+
+    if (number > 1 ) {
+
+        console.log("positive") ;
+
+    }
+    else if(number < 0) {
+        console.log("negative") ;
+    }else {
+        console.log(0);
+    }
+}
+checksign(0);
+checksign(1);
+checksign(-1);
